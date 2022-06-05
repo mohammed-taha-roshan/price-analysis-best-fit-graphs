3a9a6d8579414bb1f6c98810f7bb2f4639c377a2
mohammed-taha-roshan <106545180+mohammed-taha-roshan@users.noreply.github.com>
2022-06-05 16:07:08 +0530

    Add files via upload

---
+++ 4thbest_fitting_graph_data.py
@@ -0,0 +1,46 @@
+#  4     sqft_above
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from sklearn.model_selection import train_test_split
+
+file_name='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/FinalModule_Coursera/data/kc_house_data_NaN.csv'
+dff=pd.read_csv(file_name)
+dff=dff.drop("id",axis=1)
+dff=dff.drop("Unnamed: 0",axis=1)
+
+dff=dff.replace('?',np.nan)
+avg1=dff['sqft_above'].astype('float').mean(axis=0)
+dff=dff.replace(np.nan,avg1)
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(dff,dff,test_size=.2,random_state=4);
+
+
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(X_train[['sqft_above']])
+train_y = np.asanyarray(X_train[['price']])
+regr.fit(train_x, train_y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+
+
+plt.scatter(X_train.sqft_above, X_train.price,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("sqft_above")
+plt.ylabel("price")
+plt.show()
+print("****************************")
+test_x = np.asanyarray(X_test[['sqft_above']])
+test_y = np.asanyarray(X_test[['price']])
+test_y_ = regr.predict(test_x)
+mae= np.mean(np.absolute(test_y_ - test_y))
+mae_=mae/21613
+mse=np.mean((test_y_ - test_y) ** 2)
+mse_=mse/21613
+print("Mean absolute error: " , mae_)
+print("Residual sum of squares (MSE): " ,mse_ )
---
+++ qn2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+filename ="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+df= pd.read_csv(filename, names = headers)
+
+df['height']=df['height']/df['height'].max()
+display(df['height'])
